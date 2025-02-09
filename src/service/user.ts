cc886faa8d46ceacbc9a91dbc75d7c1b8bc3c84e
현채은 <ads4023@naver.com>
2025-02-09 22:17:36 +0900

    📝 [docs] 주석 추가
    addUser 함수 주석 추가

--- src/service/user.ts
+++ src/service/user.ts
@@ -8,6 +8,7 @@ export type OAuthUser = {
 };
 
 export async function addUser({ id, username, email, name, image }: OAuthUser) {
+  //사용자가 존재하지 않는 경우 추가
   return client.createIfNotExists({
     _id: id,
     _type: "user",
