0a6da0a057ec85751c3898858e3a6b01170e119d
현채은 <ads4023@naver.com>
2025-02-22 20:20:22 +0900

    ♻️ [refactor] : Image priority 속성 지정 (index <2)
    이미지 priority 속성 지정

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -3,19 +3,22 @@ import Image from 'next/image';
 import React from 'react';
 import CommentBar from './CommentBar';
 import PostAuthor from './PostAuthor';
-import PostIcon from './PostIcon';
+import ActionBar from './ActionBar';
 import PostContent from './PostContent';
 
+type Props = {
+  post: SimplePost,
+  priority?: boolean
+}
 
 
-
-export default function PostCard({ post }: { post: SimplePost }) {
+export default function PostCard({ post, priority = false }: Props) {
   return (
     <article className='w-full mb-5 shadow-sm shadow-neutral-300 rounded-lg text-sm'>
       <PostAuthor post={post} />
-      <Image className='w-full h-[400px] object-cover' src={post.image} width={300} height={200} alt='postImage' />
+      <Image className='w-full h-[400px] object-cover' priority={priority} src={post.image} width={300} height={200} alt='postImage' />
       <div className='p-4'>
-        <PostIcon />
+        <ActionBar />
         <PostContent post={post} />
         <CommentBar />
       </div>
--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -2,15 +2,15 @@
 import React from 'react';
 import useSWR from 'swr';
 import { SimplePost } from '@/model/Post';
-import { PulseLoader } from 'react-spinners';
 import PostCard from './PostCard';
+import { PulseLoader } from 'react-spinners';
 
 export default function PostList() {
   const { data: posts, isLoading } = useSWR<SimplePost[]>('/api/post');
   return (
     <ul className='w-full'>
-      {isLoading && <PulseLoader className='text-center mt-32' size={8} color='red' />}
-      {posts && posts.map(post => <PostCard key={post.id} post={post} />)}
+      {isLoading && <PulseLoader className='text-center mt-32' color='red' size={8} />}
+      {posts && posts.map((post, index) => <PostCard key={post.id} post={post} priority={index < 2} />)}
     </ul>
   );
 }
