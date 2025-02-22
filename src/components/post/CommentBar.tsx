439a8d86421246f67e2c2b6dcf5373c265920f1c
현채은 <ads4023@naver.com>
2025-02-22 20:19:08 +0900

    🐛 [fix] : emoji picker 오류 수정
    form 태그 관련 오류 수정

--- src/components/post/CommentBar.tsx
+++ src/components/post/CommentBar.tsx
@@ -10,16 +10,26 @@ export default function CommentBar() {
     setInputValue(prevInput => prevInput + emojiData.emoji);
     setIsOpen(false); // 선택 후 자동으로 닫히게 함
   }
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   }
-  const onEmojiClick = () => {
+
+  const onEmojiClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
     setIsOpen(prev => !prev)
   }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('제출', inputValue)
+    setInputValue('')
+  }
+
   return (
-    <form className="flex relative items-center gap-2 pt-3">
+    <form onSubmit={handleSubmit} className="flex relative items-center gap-2 pt-3">
       <input type="text" value={inputValue} onChange={handleInputChange} placeholder="댓글 달기 ..." className="w-full py-3 px-1 h-7 outline-none" />
-      <button onClick={onEmojiClick}><EmojiIcon /></button>
+      <button type="button" onClick={onEmojiClick}><EmojiIcon /></button>
       {isOpen &&
         <div className="absolute bottom-9 right-0 z-10">
           <EmojiPicker
