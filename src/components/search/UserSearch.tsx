dd4d9d3e1bb1f749eaef113d32d650f5bd159623
현채은 <ads4023@naver.com>
2025-03-03 21:18:14 +0900

    ✨ [feat] : search debounce 적용
    useDebounce hooks생성

--- src/components/search/UserSearch.tsx
+++ src/components/search/UserSearch.tsx
@@ -7,6 +7,7 @@ import { PulseLoader } from "react-spinners";
 import useSWR from "swr";
 import SearchResultCard from "./SearchResultCard";
 import { SearchUser } from "@/model/User";
+import useDebounce from "@/util/hooks";
 
 
 
@@ -16,8 +17,9 @@ export default function UserSearch() {
   // 검색하는 keyword가 없다면 /api/search -> 전체 유저를 보여줘 (ex. 처음 searchPage에 들어온 경우)
   const [isFocused, setIsFocused] = useState<boolean>(true);
   const [searchTerm, setSearchTerm] = useState<string>('')
+  const debouncedSearch = useDebounce(searchTerm, 1000)
 
-  const { data: users, isLoading } = useSWR<SearchUser[]>(`/api/search/${searchTerm}`);
+  const { data: users, isLoading } = useSWR<SearchUser[]>(() => searchTerm ? `/api/search/${debouncedSearch}` : '/api/search');
   console.log(users)
 
   const handleSearchTerm = (e: React.ChangeEvent<HTMLInputElement>) => {
