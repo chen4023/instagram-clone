b97b22f5726aad7e456aaa1bf8b3154c407313e5
현채은 <ads4023@naver.com>
2025-03-03 18:27:52 +0900

    💄 [style] : searchBar layout 수정
    css 수정 및 searchresultUI 생성

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,14 @@
 import SearchBar from '@/components/search/SearchBar';
+import SearchResult from '@/components/search/SearchResult';
 import React from 'react';
 
 export default function SearchPage() {
   return (
-    <div>
-      <h1 className='text-2xl font-semibold ml-1 my-3 mb-8'>검색</h1>
+    <div className='w-full'>
+      <h1 className='text-center text-2xl font-semibold my-3 mb-8'>검색</h1>
       <SearchBar />
-    </div>
+      <SearchResult />
+    </div >
   );
 }
 
--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -12,13 +12,13 @@ export default function SearchBar() {
     setSearchTerm(e.target.value)
   }
   return (
-    <div className="relative w-[300px]">
+    <div className="relative w-[300px] h-[40px] mx-auto">
       {!isFocused && <IoSearch className="absolute left-2 top-2.5 h-5 text-[#8E8E8E]" />}
       <input type="text" value={searchTerm}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
         onChange={handleSearchTerm} className={`bg-[#EFEFEF] ${!isFocused ? 'px-8' : 'px-3'} w-[300px] h-[40px] rounded-md outline-none`} placeholder="검색" />
-      {isFocused && <IoCloseCircle className="relative bottom-[31px] h-6 left-[275px] hover:cursor-pointer text-[#8E8E8E]" onMouseDown={(e) => {
+      {isFocused && <IoCloseCircle className="relative bottom-[31px] h-5 left-[275px] hover:cursor-pointer text-[#8E8E8E] " onMouseDown={(e) => {
         e.preventDefault();
         setSearchTerm('')
       }} />}
