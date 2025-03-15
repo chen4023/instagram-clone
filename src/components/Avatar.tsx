2d3be27e8432de0e72df11cff21cabbe693ea01c
chen4023 <ads4023@naver.com>
2025-03-15 18:58:03 +0900

    ♻️ [refactor] : Avata 컴포넌트 리팩토링
    2개로 분리하여 switch case문 사용 -> 하나의 switch case 문으로 refactor

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+type AvatarSize = 'small' | 'normal' | 'large' | 'xlarge'
 type Props = {
   image?: string | null
-  size?: 'small' | 'normal' | 'large' | 'xlarge'
+  size?: AvatarSize
   highlight?: boolean
 }
 export default function Avatar({ image, size = 'large', highlight = false }: Props) {
   return (
     <div className={getContainerStyle(size, highlight)}>
       {/* eslint-disable-next-line @next/next/no-img-element*/}
-      <img className={`rounded-full object-cover bg-white p-2 ${getImageSizeStyle(size)}`}
+      <img className={`rounded-full object-cover bg-white p-2 ${getImageSizeStyle(size).image}`}
         src={image ?? undefined}
         alt='User Profile'
         referrerPolicy='no-referrer' />
@@ -16,42 +17,32 @@ export default function Avatar({ image, size = 'large', highlight = false }: Pro
   );
 }
 
-function getContainerStyle(size: string, highlight: boolean): string {
+type ImageSizeStyle = {
+  container: string;
+  image: string
+}
+function getContainerStyle(size: AvatarSize, highlight: boolean): string {
   const baseStyle = 'rounded-full flex justify-center items-center';
   const highlightStyle = highlight ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300' : '';
-  let sizeStyle;
-  switch (size) {
-    case 'small':
-      sizeStyle = 'w-9 h-9'
-      break
-    case 'normal':
-      sizeStyle = 'w-[48px] h-[48px]'
-      break
-    case 'large':
-      sizeStyle = 'w-[68px] h-[68px]'
-      break
-    case 'xlarge':
-      sizeStyle = 'w-[120px] h-[120px]'
-      break
-    default:
-      sizeStyle = ''
-      break
-  }
+  const { container } = getImageSizeStyle(size)
   // const sizeStyle = size === 'small' ? 'w-9 h-9' : 'w-[68px] h-[68px]'
-  return `${baseStyle} ${highlightStyle} ${sizeStyle}`
+  return `${baseStyle} ${highlightStyle} ${container}`
 }
 
-function getImageSizeStyle(size: string): string {
+function getImageSizeStyle(size: AvatarSize): ImageSizeStyle {
   switch (size) {
     case 'small':
-      return 'w-[34px] h-[34px] p-[0.1rem]'
+      return { container: 'w-9 h-9', image: 'w-[34px] h-[34px] p-[0.1rem]' }
     case 'normal':
-      return 'w-[44px] h-[44px] p-[0.15rem]'
+      return { container: 'w-[48px] h-[48px]', image: 'w-[44px] h-[44px] p-[0.15rem]' }
+
     case 'large':
-      return 'w-16 h-16 p-[0.2rem]'
+      return { container: 'w-[68px] h-[68px]', image: 'w-16 h-16 p-[0.2rem]' }
+
     case 'xlarge':
-      return 'w-[116px] h-[116px] p-[0.25rem]'
+      return { container: 'w-[142px] h-[142px]', image: 'w-[138px] h-[138px] p-[0.3rem]' }
+
     default:
-      return ''
+      throw new Error(`Unsupported type size : ${size}`)
   }
 }
